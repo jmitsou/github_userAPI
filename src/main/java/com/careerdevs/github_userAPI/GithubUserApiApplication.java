f4932d90a11e08404e696ad053163af70ccf31b2
Jeff Mitsou <shinetachi88@gmail.com>
2021-11-30 18:01:39 -0500

    Initial commit for project

---
+++ src/main/java/com/careerdevs/github_userAPI/GithubUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.github_userAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUserApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/github_userAPI/GithubUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.github_userAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
